1743ee8c0ed27d623bffa04ac834bf8804232f99
Rick Behl <rickbehl@hotmail.com>
2017-07-11 22:24:43 +0800

    End of part 5

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import {Bond, TimeBond} from 'oo7';
 import {Rspan, Rimg} from 'oo7-react';
-import {InputBond, HashBond} from 'parity-reactive-ui';
-import {bonds, formatBalance} from 'oo7-parity';
+import {InputBond, HashBond, BButton, TransactButton} from 'parity-reactive-ui';
+import {bonds, formatBalance, isNullData} from 'oo7-parity';
 import {GitHubHintABI} from 'oo7-parity';
 
 export class App extends React.Component {
 	constructor() {
 		super();
-		this.bond = new Bond();
-		this.GithubHint = bonds.makeContract(bonds.registry.lookupAddress('gihubhint', 'A'), GitHubHintABI);
+		this.name = new Bond;
+		this.recipient = bonds.registry.lookupAddress(this.name, 'A');
 	}
 	render() {
 		return (
 			<div>
-<InputBond bond={this.bond} placeholder='Name' />
-	<Rimg src={this.GithubHint.entries(bonds.registry.lookupData(this.bond, 'IMG'))[0]} />
+				My balance: <Rspan>
+							{bonds.balance(bonds.me).map(formatBalance)}
+				</Rspan>
+				<br />
+				<InputBond bond={this.name} placeholder='Name of recipient' />
+				<TransactButton
+						content={this.name.map(n => `Give ${n} 10 Finney`)}
+						disabled={this.recipient.map(isNullData)}
+						tx={{
+							to: this.recipient,
+							value: 10 * 1e15
+						}}
+				/>
 			</div>
 		)
 	}
