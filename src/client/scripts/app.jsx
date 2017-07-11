08dbe18f746d8dfcb6e0cc3a0ee7fb0cff7499e4
Rick Behl <rickbehl@hotmail.com>
2017-07-11 18:15:03 +0800

    End of part 4

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,38 +1,21 @@
 import React from 'react';
 import {Bond, TimeBond} from 'oo7';
-import {Rspan} from 'oo7-react';
-import {InputBond} from 'parity-reactive-ui';
+import {Rspan, Rimg} from 'oo7-react';
+import {InputBond, HashBond} from 'parity-reactive-ui';
 import {bonds, formatBalance} from 'oo7-parity';
+import {GitHubHintABI} from 'oo7-parity';
 
 export class App extends React.Component {
 	constructor() {
 		super();
 		this.bond = new Bond();
-		this.time = new TimeBond;
-		window.bonds = bonds;
+		this.GithubHint = bonds.makeContract(bonds.registry.lookupAddress('gihubhint', 'A'), GitHubHintABI);
 	}
 	render() {
 		return (
 			<div>
-				Current block author's balance is: &nbsp;
-				<Rspan style={{fontWeight: 'bold'}}>
-						{bonds.balance(bonds.head.author).map(formatBalance)}
-				</Rspan>
-				<br/>
-				<br/>				
-				Accounts available:&nbsp;
-				<Rspan>
-						{bonds.accounts.map(_=>_.join(', '))}
-				</Rspan>
-				<br/>
-				<br/>
-				Default account:&nbsp;
-				<Rspan>{bonds.me}</Rspan>
-				<br/>Given the name of&nbsp;<Rspan>{bonds.accountsInfo[bonds.me].name}</Rspan>
-				<br/>With a balance of&nbsp;
-				<Rspan>
-						{bonds.balance(bonds.me).map(formatBalance)}
-				</Rspan>
+<InputBond bond={this.bond} placeholder='Name' />
+	<Rimg src={this.GithubHint.entries(bonds.registry.lookupData(this.bond, 'IMG'))[0]} />
 			</div>
 		)
 	}
