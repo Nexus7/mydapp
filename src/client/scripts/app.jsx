d53ada90e33c6b9a8ce4d226a8f8e8571b4b5189
Rick Behl <rickbehl@hotmail.com>
2017-07-12 16:18:18 +0800

    End of part 8

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,31 +1,54 @@
 import React from 'react';
 import {Bond, TimeBond} from 'oo7';
-import {Rspan, Rimg} from 'oo7-react';
-import {InputBond, HashBond, BButton, TransactButton} from 'parity-reactive-ui';
+import {Rspan, Rimg, ReactiveComponent} from 'oo7-react';
+import {InputBond, HashBond, BButton, TransactionProgressLabel} from 'parity-reactive-ui';
 import {bonds, formatBalance, isNullData} from 'oo7-parity';
 import {GitHubHintABI} from 'oo7-parity';
 
 const CounterABI = [{"constant":false,"inputs":[{"name":"_option","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}];
 const Options = ['Red', 'Green', 'Blue'];
 
+class VoteOption extends ReactiveComponent {
+	constructor() {
+		super(['votes', 'enabled']);
+	}
+	readyRender() {
+		var s = {float: 'left', minWidth: '3em'};
+		if (!this.state.enabled) 
+				s.cursor = 'not-allowed';
+		return (<span style={{ borderLeft:
+				`${1 + this.state.votes * 10}px black solid` }}>
+				<a 
+						style={{float: 'left', minWidth: '3em'}}
+						href='#'
+						onClick={this.state.enabled && this.props.vote}>
+										{this.props.label}
+				</a>
+		</span>);
+		}
+};
+
 export class App extends React.Component {
 	constructor() {
 		super();
-		this.counter = bonds.makeContract('0x7aC77Cb854E064f22E747F40b90FE6D6Bc1e3197', CounterABI);	}
+		this.counter = bonds.makeContract('0x7aC77Cb854E064f22E747F40b90FE6D6Bc1e3197', CounterABI);
+		this.state = { tx: null };
+		this.voted = this.counter.hasVoted(bonds.me);
+	}
 	render() {
+		var votingEnabled = Bond.all([this.voted, this.state.tx])
+			.map(([v, t]) => !v && (!t || !!t.failed));		
 		return (
 			<div>
-				{Options.map((n, i) => (<div key={i}>
-						<Rspan style={{
-									borderLeft: this.counter
-											.votes(i)
-											.map(v => `${1 + v * 10}px black solid`)
-						}}>
-									<span style={{float: 'left', minWidth: '3em'}}>
-											{n}
-									</span>
-						</Rspan>
-				</div>))}
+				{Options.map((n, i) => (<div key={i}><VoteOption
+						label={n}
+						votes={this.counter.votes(i)}
+						vote={() => this.setState({tx: this.counter.vote(i)})}
+						enabled={votingEnabled}
+				/></div>))}
+				<div style={{marginTop: '1em'}}>
+						<TransactionProgressLabel value={this.state.tx}/>
+				</div>
 			</div>);
 	}
-}
+};
